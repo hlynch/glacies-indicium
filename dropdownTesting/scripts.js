ce1b86a86e588619ef65b304f738cada2bb6d57c
zachspiel <zachspielberger@gmail.com>
2021-02-09 15:35:18 -0700

    Updated dropdown menu in testing folder to work with any amount of regions

--- dropdownTesting/scripts.js
+++ dropdownTesting/scripts.js
@@ -1,3 +1,153 @@
+const json = [
+  {
+    name: 'Northern Victoria Land',
+    subregions: [
+      {
+        name: 'Asgard Range',
+        subregions: [
+          {
+            name: 'Mt. Newall',
+          },
+          {
+            name: 'Round Mountain',
+          },
+        ],
+      },
+      {
+        name: 'Ferrar Glacier',
+        level: 1,
+        subregions: [
+          {
+            name: 'Kukri Hills',
+          },
+          {
+            name: 'Briggs Hills',
+          },
+          {
+            name: 'Thomas Heights',
+          },
+        ],
+      },
+      {
+        name: 'Kukri Hills',
+        subregions: [
+          {
+            name: 'Mt Coates',
+          },
+        ],
+      },
+      {
+        name: 'Quartermain Mountain',
+        subregions: [
+          {
+            name: 'Beacon Valley',
+          },
+          {
+            name: 'Pivot Peak',
+          },
+        ],
+      },
+      {
+        name: 'Royal Society Range',
+        subregions: [
+          {
+            name: 'Garwood Valley',
+          },
+          {
+            name: 'Miers Valley',
+          },
+          {
+            name: 'Heald Island',
+          },
+          {
+            name: 'Blue Glacier',
+          },
+          {
+            name: 'Mt. Rucker',
+          },
+          {
+            name: 'Trough Lake',
+          },
+          {
+            name: 'Mt. Huggins',
+          },
+          {
+            name: 'The Spire',
+          },
+          {
+            name: 'Table Mountain',
+          },
+        ],
+      },
+      {
+        name: 'Taylor Glacier',
+        subregions: [{ name: 'Northwest Mountain' }],
+      },
+      {
+        name: 'Taylor Valley',
+        subregions: [
+          {
+            name: 'Northwest Mountain',
+          },
+          {
+            name: 'New Harbor',
+          },
+          {
+            name: 'Fryxell Basin',
+          },
+          {
+            name: 'Kukri Hills',
+          },
+          {
+            name: 'Hoare Basin',
+          },
+        ],
+      },
+      {
+        name: 'Willett Range',
+        subregions: [
+          { name: 'Head Mountains' },
+          { name: 'Apocolypse Peaks' },
+          { name: 'Shapeles Mountain' },
+        ],
+      },
+      {
+        name: 'Wright Valley',
+        subregions: [{ name: 'Mt. Fleming' }],
+      },
+    ],
+  },
+];
+
+function parseData(jsonObject) {
+  let listRoot = document.createElement('ul');
+  for (var index = 0; index < jsonObject.length; index++) {
+    listRoot.appendChild(createListElement(jsonObject[index].name));
+    if (regionHasSubregions(jsonObject, index)) {
+      const subregionList = parseData(jsonObject[index].subregions);
+      listRoot.appendChild(subregionList);
+    }
+  }
+  return listRoot;
+}
+
+function regionHasSubregions(array, currentIndex) {
+  return (
+    array[currentIndex].subregions !== undefined &&
+    array[currentIndex].subregions.length > 0
+  );
+}
+
+function createListElement(content) {
+  const listElement = document.createElement('li');
+  listElement.innerHTML = content;
+  return listElement;
+}
+
+const regionList = parseData(json);
+document.getElementById('root').appendChild(regionList);
+
+/* 
 const fs = require('fs');
 
 fs.readFile('./regionList.json', 'utf8', (err, jsonString) => {
@@ -17,7 +167,8 @@ function parseData(regionList) {
     )
       console.log('  ' + regionList[index].subregions[innerIndex].name);
   }
-}
+} */
+
 /*
 class UserInterface {
   constructor(dropdownContainerId, bandContainerId) {
