b173fc78834cc0eeafeda07eb6d2b7a67b0fd55a
Logan Garrett <m.logan.garrett@gmail.com>
2021-03-04 17:36:00 -0800

    implemented rough regional identification functionality.

--- prep_scripts/raster-prep.py
+++ prep_scripts/raster-prep.py
@@ -2,6 +2,11 @@ from pathlib import Path
 import rasterio
 import argparse
 import subprocess
+import csv
+import json
+import haversine
+from pyproj import Transformer
+from pprint import pprint
 
 # TODO:
 # - logic to read in the csv from mark containing the regional key info
@@ -11,6 +16,9 @@ import subprocess
 
 # BAND EXTRACTION FUNCTION #
 def band_extract(data_folder, output_folder):
+
+    print("####### BAND EXTRACTION ROUTINE #######") 
+
     ##################################
     # DEFINE THE BAND KEY DICTIONARY #
     ##################################
@@ -30,11 +38,10 @@ def band_extract(data_folder, output_folder):
     # make a pathlist of paths to each geotiff in the data folder
     pathlist = Path(data_folder).glob('*.tif')
 
+    print("reading geotiffs in the data folder...")
+
     # iterate over each geotiff in the provided data folder
     for path in pathlist:
-
-
-        print("reading geotiffs in the data folder...")
 
         ##############################
         # PRELIMINARY INFO GATHERING #
@@ -85,12 +92,74 @@ def band_extract(data_folder, output_folder):
             do_translate = subprocess.run(["gdal_translate", "-b", str(band), str(path), str(outfile_path)], stdout=subprocess.DEVNULL)
 
 # REGION KEYING FUNCTION
-def rekey_by_region(data_folder, output_folder):
+def rekey_by_region(data_folder, output_folder, region_centers):
     # INGEST MARK'S CSV WITH REGIONAL INFORMATION
     # FOR EACH REGION LEVEL:
         # IF RASTER BOUNDS ARE WITHIN REGIONAL BOUNDS:
             # APPEND REGION NAME + "_" SEPARATOR TO OUTFILE_NAME
     # END RESULT E.G. "NorthernVictoriaLand_McMurdoDryValleys_TaylorValley_KukriHills"
+
+    print("####### REGIONAL IDENTIFICATION ROUTINE #######") 
+
+    # make a pathlist of paths to each geotiff in the data folder
+    pathlist = Path(data_folder).glob('*.tif')
+
+    print("reading geotiffs in the data folder...")
+    # iterate over each geotiff in the provided data folder
+    for path in pathlist:
+
+        ##############################
+        # PRELIMINARY INFO GATHERING #
+        ##############################
+        # extract the filename
+        filename = str(path.stem)
+        filepath = str(path)
+
+        # print the name of and path to this file
+        print('\t' + filepath)
+
+        # REGIONAL PROCESSING 
+        # open the file with rasterio
+        raster = rasterio.open(path)
+
+        # get the center coordinates of this raster
+        # these values are projected in EPSG 3031 (antarctic polar stereographic)
+        centerX, centerY = raster.xy(raster.height // 2, raster.width // 2)
+
+        # get the source raster's projection code
+        inProjection = str(raster.crs)
+
+        # "unproject" these center coordinates into spherical latitude and longitude
+        # which has epsg code 4326
+        outProjection = 'epsg:4326'
+
+        transformer = Transformer.from_crs(inProjection, outProjection)
+        centerLat, centerLon = transformer.transform(centerX, centerY)
+
+        #print('\t\tunprojected center: ' + str(centerLat) + ', ' + str(centerLon))
+
+        # structure to hold regions with their distances to this raster
+        distances_matrix = []
+
+        # visit each region in region_centers
+        # at each region, add a row to the distances matrix which has
+        # the region name and the distance from this image to that region
+
+        for region in region_centers:
+            distance = haversine.distance([centerLat, centerLon], [float(region[1]), float(region[2])])
+            distances_matrix.append([region[0], distance])
+
+        #print("\t\t\tdistance matrix:")
+        #for region in distances_matrix:
+            #print('\t\t\t\t{:<20s} {:<10s}'.format(region[0], str(region[1])))
+
+        # distances_matrix now knows how far this image's center is from all the regions
+        # if we sort the matrix by its second column, the first row will be the nearest region
+        distances_matrix = sorted(distances_matrix, key=lambda x: x[1])
+
+        print("\t\tnearest region: ")
+        print("\t\t\t" + distances_matrix[0][0] + "\t" + "{:.2f}".format(distances_matrix[0][1]) + " km")
+
     return
 
 # MOSAIC GENERATION FUNCTION
@@ -103,6 +172,35 @@ def build_mosaics(data_folder, output_folder):
                 # MOSAICS OF Cs TO COVER Bs, AND
                     # MOSAICS OF Bs TO COVER As
     return
+
+# ingests a three-column csv of region, lat, lon into a list of
+# rows[i][j] where len(i)=len(csv)-1 and len(j)=3 (name, lat, lon)
+def ingest_L4_data(region_key_file):
+    cols = []
+    rows = []
+
+    with open(region_key_file, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+
+        # advances the reader one position to read the first row
+        # which contains the column titles
+        cols = next(csvreader)
+
+        # iterates across the rest of the rows and adds them to a list
+        for row in csvreader:
+            rows.append(row)
+
+        # could also return the col titles from row 1 but they don't really matter
+        return rows
+    return
+
+# ingests a json file representing the hierarchy of regions
+# for file keying purposes
+def ingest_regional_json(json_regions_file):
+    with open(json_regions_file) as f:
+        regional_hierarchy = json.load(f)
+
+    return regional_hierarchy
 
 #################
 # Main Function #
@@ -119,21 +217,40 @@ def main():
     data_folder = Path(args.data_folder)
     output_folder = Path(args.output_folder)
 
+    # ingest a 3-column csv containing L4 regions and their center lat/lon coordinates
+    # store this data in region_centers
+    region_key_file = 'L4_regions_sparse.csv'
+    region_centers = ingest_L4_data(region_key_file)
+    print("found 68 regions in  key file")
+
+    # region_centers[i][j] can now access a region name and its lat/lon where
+    # i<len(region_centers) and 1<=j<=2
+
+    # TODO: THE REGIONS JSON FILE IS CURRENTLY IN A SOMEWHAT FUCKED-UP STATE
+    # AND SHOULDN'T BE USED
+    # ingest the json with the full regional hierarchy
+    #json_regions_file = 'regions.json'
+    #regional_hierarchy = ingest_regional_json(json_regions_file)
+
+    # regional_hierarchy now has the structured hierarchy info
+    # we can use this to build fully-qualified filename strings by walking
+    # the json tree from the leaves up to the root
+
     ############################
     # MAIN PREPROCESSING STEPS #
     ############################
     # pass the input/output folders to functions to perform the preprocessing steps
 
-    # band extraction
-    band_extract(data_folder, output_folder)
+    # band extraction (uncomment to run)
+    #band_extract(data_folder, output_folder)
 
-    # region keying
-    rekey_by_region(data_folder, output_folder)
+    # region keying (uncomment to run)
+    rekey_by_region(data_folder, output_folder, region_centers)
 
-    # mosaic generation
-    build_mosaics(data_folder, output_folder)
+    # mosaic generation (uncomment to run)
+    #build_mosaics(data_folder, output_folder)
 
-
+    # the script is done
     print("goodbye")
 
 
